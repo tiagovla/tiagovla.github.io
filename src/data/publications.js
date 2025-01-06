867c0468b3b17a3e9eeea4880e2ac6a050fb843f
tiagovla <tiagovla@gmail.com>
2025-01-06 16:58:27 -0300

    fix: add data publications

---
+++ src/data/publications.js
@@ -0,0 +1,5 @@
+const publications = [
+    'Tiago V. L. Amorim “A Plug-and-Play DGTD Implementation for General Dispersive Media,” 2024 IEEE AP-S International Symposium on Antennas and Propagation, Florence, Italia, July 2024.',
+]
+
+export default publications
