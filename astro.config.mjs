bf1974e0c7f9d266c32b1a899803e5deb0130aa3
tiagovla <tiagovla@gmail.com>
2025-01-06 16:02:23 -0300

    feat: initial structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,24 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+
+import icon from 'astro-icon'
+
+import tailwind from '@astrojs/tailwind'
+
+import remarkMath from 'remark-math'
+import rehypeKatex from 'rehype-katex'
+
+import react from '@astrojs/react'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [icon(), tailwind(), react()],
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+    shikiConfig: {
+      theme: 'one-dark-pro', // Make sure this is the theme you want
+      wrap: true, // Optional: wrap long lines
+    },
+  },
+})
---
+++ src/data/navData.js
@@ -0,0 +1,16 @@
+const navData = [
+  {
+    name: 'Blog',
+    path: '/blog/',
+  },
+  {
+    name: 'Publications',
+    path: '/publications/',
+  },
+  {
+    name: 'Resume',
+    path: '/resume.pdf',
+  },
+]
+
+export default navData
