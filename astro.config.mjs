bd719b52ce9921ef9a68ed31ea8a685f125af803
tiagovla <tiagovla@gmail.com>
2025-07-18 01:16:45 -0300

    feat: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from 'astro/config'
 
+import sitemap from '@astrojs/sitemap'
+
 export default defineConfig({
+    site: 'https://tiagovla.github.io',
     markdown: {
         shikiConfig: {
             theme: 'github-light',
         },
     },
+
+    integrations: [sitemap()],
 })
