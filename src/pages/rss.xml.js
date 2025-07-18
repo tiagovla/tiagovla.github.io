fc9e907c46a3956d5baa36b48680fd3c6a501b50
tiagovla <tiagovla@gmail.com>
2025-07-18 03:43:20 -0300

    feat: add rss support

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,17 @@
+import rss from '@astrojs/rss'
+import { getCollection } from 'astro:content'
+
+export async function GET(context) {
+    const blog = await getCollection('blog')
+    return rss({
+        title: 'tiagovla.github.io',
+        description: "Feed of tiagovla.github.io's posts",
+        site: context.site,
+        items: blog.map((post) => ({
+            title: post.data.title,
+            pubDate: post.data.date,
+            description: post.data.description,
+            link: `/blog/posts/${post.id}/`,
+        })),
+    })
+}
